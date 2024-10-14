21665ff20d433d25a13412832af7242049c930f5
Felipe <felipesakaiipolito@gmail.com>
2024-10-14 15:31:23 -0400

    Ex1 e 2

---
+++ Ex1/Ex1/Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿static bool PertenceFibo(int n)
+{
+    int a = 0, b = 1, c = 0;
+
+    if (n == 0 || n == 1) return true;
+
+    while (c < n)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return c == n;
+}
+
+Console.WriteLine("Digite um numero para verificar a sequencia de Fibonacci: ");
+int n = int.Parse(Console.ReadLine());
+
+if (PertenceFibo(n))
+{
+    Console.WriteLine($"{n} Pertence a seq Fibonacci. ");
+
+}
+else
+{
+    Console.WriteLine($"{n} Nao Pertence a seq Fibonacci. ");
+}
+
+
---
+++ Ex2/Ex2/Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿string frase1 = "O rato roeu a roupa do rei de roma";
+char letra = 'a';
+int contador = 0;
+
+foreach(char c in frase1)
+{
+    if (c == 'a' || c == 'A') contador++;
+}
+
+Console.WriteLine(contador > 0
+            ? $"A letra 'a' aparece {contador} vezes no texto."
+            : "A letra 'a' não está presente no texto.");
